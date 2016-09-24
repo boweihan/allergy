1294310c64f25d8fcd7e26f28f8b8c19041c613c
bowei <bowei.han100@gmail.com>
2016-09-24 16:45:50 -0400

    added API end points to search for products by ingredient and to search for ingredient by product

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,26 @@ class PagesController < ApplicationController
     @ingredients = Ingredient.all
     render json: @ingredients
   end
+  
+  def cross_reference
+    #look through params for products being posted
+    #loop through those products
+    #check DB to find where product = params[product]
+    #if product exists, grab ingredients and store in ingredients array
+    #for each subarray in ingredients array, find the ones in common (uniq?)
+    #render those ingredents as JSON
+  end
+  
+  def search_products
+    params[:ingredient] = "Lanolin"
+    @products = Ingredient.where(name: params[:ingredient]).first.products
+    render json: @products
+  end
+  
+  def search_ingredients
+    params[:product] = "AFM Safecoat WaterShield"
+    @ingredients = Product.where(name: params[:product]).first.ingredients
+    render json: @ingredients
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/products' => 'pages#products'
   get '/ingredients' => 'pages#ingredients'
-  
+  get '/cross_reference' => 'pages#cross_reference'
+  get '/search_products' => 'pages#search_products'
+  get '/search_ingredients' => 'pages#search_ingredients'
 end
